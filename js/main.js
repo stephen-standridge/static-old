029488df3cdff027f6285d5d0aded82472b5c79b
stephen-standridge <stephen@formisfunction.io>
2015-03-28 11:08:56 -0700

    flickering granular static

--- js/main.js
+++ js/main.js
@@ -72,7 +72,7 @@
     function animate() {
         requestAnimationFrame( animate );
         // cameras.controls.update()
-        materials.updateTime('increase', 0.05)
+        materials.updateTime('increase', 0.005)
         renderer.render( scene, cameras.getCurrent() );
     }
     function onWindowResize( event ) {
